d3f9885f4eb7719896bc1b0a392cc09190cb5f8c
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-10-14 18:30:22 -0500

    configure file for userdata

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "proxmox_virtual_environment_vm" "vm" {
 
   initialization {
     datastore_id      = var.datastore_id
-    user_data_file_id = ""
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config
   }
 
   disk {
