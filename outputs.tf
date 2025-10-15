87b2cbf8cbbc06d0c422dff157d29793e204de52
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-10-15 15:11:22 -0500

    add hostname to the output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "ipv4_addresses" {
+output "vm_ipv4_addresses" {
     value       = proxmox_virtual_environment_vm.vm.ipv4_addresses
     description = "IP addresses assigned to the Proxmox Virtual Machine"
+}
+
+output "vm_hostname" {
+    value       = var.vm_hostname
+    description = "Hostname of the Proxmox Virtual Machine"
 }
